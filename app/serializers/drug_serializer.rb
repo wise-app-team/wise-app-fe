b1609b80832c13f81029e50ea8dfe11e17d6fbdf
Meredith Trevino <meredithtrevino3@gmail.com>
2023-04-13 19:22:01 -0600

    commented out serializer

--- app/serializers/drug_serializer.rb
+++ app/serializers/drug_serializer.rb
@@ -1,18 +1,18 @@
 # frozen_string_literal: true
 
-class DrugSerializer
-  include JSONAPI::Serializer
-  attributes :name, :synonym
+# class DrugSerializer
+#   include JSONAPI::Serializer
+#   attributes :name, :synonym
 
-  def self.format_drugs(drugs)
-    { data: drugs.map do |drug|
-              {
-                id: drug.id.to_s,
-                attributes: {
-                  name: drug.name,
-                  synonym: drug.synonym
-                }
-              }
-            end }
-  end
-end
+#   def self.format_drugs(drugs)
+#     { data: drugs.map do |drug|
+#               {
+#                 id: drug.id.to_s,
+#                 attributes: {
+#                   name: drug.name,
+#                   synonym: drug.synonym
+#                 }
+#               }
+#             end }
+#   end
+# end
