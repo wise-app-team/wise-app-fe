4b445d9b468a104ed6cbe0b6ae10c3bf2be90284
Meredith Trevino <meredithtrevino3@gmail.com>
2023-04-14 11:43:02 -0600

    adds serializer

--- app/serializers/drug_serializer.rb
+++ app/serializers/drug_serializer.rb
@@ -1,22 +1,16 @@
-#from internet: 
-# class DrugSerializer
-#   include FastJsonapi::ObjectSerializer
-#   attributes :name, :description, :created_at, :updated_at
-# end
+class DrugSerializer
+  include JSONAPI::Serializer
+  attributes :name, :synonym
 
-# class DrugSerializer
-
-#   attributes :name, :synonym
-
-#   def self.format_drugs(drugs)
-#     { data: drugs.map do |drug|
-#               {
-#                 id: drug.id.to_s,
-#                 attributes: {
-#                   name: drug.name,
-#                   synonym: drug.synonym
-#                 }
-#               }
-#             end }
-#   end
-# end
+  def self.format_drugs(drugs)
+    { data: drugs.map do |drug|
+              {
+                id: drug.id.to_s,
+                attributes: {
+                  name: drug.name,
+                  synonym: drug.synonym
+                }
+              }
+            end }
+  end
+end
