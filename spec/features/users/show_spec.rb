df56a09540b0b3a43d136b09fd6acf5a010939a2
Meredith Trevino <meredithtrevino3@gmail.com>
2023-04-18 12:55:40 -0600

    all tests passing

--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 
 RSpec.describe 'User Show Page' do
   describe "As a visitor, when I visit /users/user_id" do
-    xit "I see the name of the app at the top of the page & links to all pages" do
+    it "I see the name of the app at the top of the page & links to all pages" do
       visit '/users/1'
 
-      stub_request(:post, "http://localhost:3000/api/v1/users").
+      stub_request(:post, "http://localhost:5000/api/v1/users").
       with(
         body: {"birthday"=>"04/05/1975", "email"=>"Pedro@pedro.com", "first_name"=>"Pedro", "last_name"=>"Pascal", "password"=>"password123", "password_confirmation"=>"password123", "phone_number"=>"555-555-5555", "street_address"=>"123 Main St", "city"=>"Denver", "state"=>"CO", "zip_code"=>"80209"},
         headers: {
